444dc197fc23f514281c7500924d4cfecec9d167
Phillip <pgkb78@umsystem.edu>
2023-02-01 21:46:53 -0600

    made backend folder, install express/nodemon

---
+++ src/pages/OnlineOrdering/Backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Royal Red Shrimp',
+      slug: 'shellfish',
+      category: 'Seafood',
+      image: '/images/RoyalRed.jpg', //try 679 x 829 images
+      price: 13.99,
+      countInStock: 500,
+      rating: 4.5,
+      numReviews: 15,
+      description: 'price per pound',
+    },
+
+    {
+      name: 'Snow Crab Legs',
+      slug: 'crab',
+      category: 'Seafood',
+      image: '/images/SnowCrab.jpg',
+      price: 11.99,
+      countInStock: 500,
+      rating: 4.5,
+      numReviews: 20,
+      description: 'price per pound',
+    },
+
+    {
+      name: 'Lobster Tails',
+      slug: 'lobster',
+      category: 'Seafood',
+      image: '/images/LobsterTails.jpg',
+      price: 9.99,
+      countInStock: 500,
+      rating: 4.5,
+      numReviews: 10,
+      description: '5-6oz tails',
+    },
+
+    {
+      name: 'Crawfish',
+      slug: 'shellfish',
+      category: 'Seafood',
+      image: '/images/crawfish.jpg',
+      price: 9.99,
+      countInStock: 500,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Straight from Louisiana',
+    },
+  ],
+};
+
+export default data;
