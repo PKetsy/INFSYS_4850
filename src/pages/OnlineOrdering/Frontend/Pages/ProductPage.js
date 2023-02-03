b983635998dc8b285ebd14616022024ca9621a38
Phillip <pgkb78@umsystem.edu>
2023-02-03 15:18:03 -0600

    tweaking /products/slug on /onlineorder

--- src/pages/OnlineOrdering/Frontend/Pages/ProductPage.js
+++ src/pages/OnlineOrdering/Frontend/Pages/ProductPage.js
@@ -1,11 +1,83 @@
+import axios from 'axios';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Rating from '../../../../components/OnlineProducts/Rating';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function ProductPage() {
   const params = useParams();
   const { slug } = params;
-  return (
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
     <div>
-      <h1>{slug}</h1>
+      <Row>
+        <Col md={6}>
+          <img
+            className="img-large"
+            src={product.image}
+            alt={product.name}
+          ></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+          </ListGroup>
+
+          <ListGroup.Item>
+            <Rating
+              rating={product.rating}
+              numReviews={product.numReviews}
+            ></Rating>
+          </ListGroup.Item>
+          <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+          <ListGroup.Item>
+            Description:
+            <p>{product.description}</p>
+          </ListGroup.Item>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
     </div>
   );
 }
